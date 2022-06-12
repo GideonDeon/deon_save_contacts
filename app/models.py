47315f959fb90780866a29ba7608cee8d67eb09d
gideon <gideong351@gmail.com>
2022-06-12 07:15:19 +0100

    update

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Contact(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, null=True)
     initials = models.CharField(max_length=1, blank=True)
     firstname = models.CharField(max_length=200, blank=True)
     lastname = models.CharField(max_length=200, blank=True)
