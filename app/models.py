b2fe23d311a2e221a1436db0a5d2d7d37b8518c2
gideon <gideong351@gmail.com>
2022-06-12 07:28:24 +0100

    upddate

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import _tkinter
 
 class Contact(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
